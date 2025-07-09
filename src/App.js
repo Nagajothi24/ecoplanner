5a574c42fe3c9c7e3885e8a155f653bded53adc0
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-07-09 17:18:41 +0530

    Updated for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import habits from './data/habits';
+import HabitCard from './components/HabitCard';
+import Dashboard from './components/Dashboard';
+import StreakTracker from './components/StreakTracker';
+import ThemeToggle from './components/ThemeToggle';
 function App() {
+  const today = new Date().toISOString().split("T")[0]; // e.g., '2025-07-08'
+  const lastDate = localStorage.getItem('lastDate');
+
+  if (lastDate !== today) {
+    localStorage.removeItem('ecoHabits'); // Reset checked habits
+    localStorage.setItem('lastDate', today); // Update to today
+  }
+
+  
+  const [checkedHabits, setCheckedHabits] = useState(() => {
+    const saved = localStorage.getItem('ecoHabits');
+    return saved ? JSON.parse(saved) : {};
+  });
+  const [theme, setTheme] = useState('light');
+
+useEffect(() => {
+  document.body.className = theme;
+}, [theme]);
+
+
+  useEffect(() => {
+    localStorage.setItem('ecoHabits', JSON.stringify(checkedHabits));
+  }, [checkedHabits]);
+
+  const toggleHabit = (id) => {
+    setCheckedHabits((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeToggle theme={theme} setTheme={setTheme} />
+
+      <h1>🌿 EcoPlanner</h1>
+      <p>Track your zero-waste habits daily!</p>
+       {/* Dashboard component */}
+    <Dashboard habits={habits} checkedHabits={checkedHabits} />
+    <StreakTracker checkedHabits={checkedHabits} />
+      {habits.map((habit) => (
+        <HabitCard
+          key={habit.id}
+          habit={habit}
+          isChecked={checkedHabits[habit.id]}
+          onToggle={toggleHabit}
+        />
+      ))}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/data/habits.js
@@ -0,0 +1,8 @@
+const habits = [
+    {id:1,name:"Use Resuable Bag"},
+    {id:2,name:"Avoid Plastic"},
+    {id:3,name:"Turn Off Lights"},
+    {id:4,name:"Use Public Transport"},
+    {id:5,name:"Recycle Waste"},
+];
+export default habits;
